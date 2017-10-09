0250ce378cb7c7c4f73186478d4ce957f3cdd1e8
Daniel Wojdak <daniel.wojdak@impaqgroup.com>
2017-10-09 14:46:29 +0200

    Inicjalizacja

---
+++ BlockIdConverter/BlockIdConverter.cpp
@@ -0,0 +1,17 @@
+// BlockIdConverter.cpp: Definiuje punkt wejścia dla aplikacji konsolowej.
+//
+
+#include "stdafx.h"
+
+struct {
+	uint32_t id;
+	std::string name;
+	bool hasItem;
+	std::string className;
+};
+
+int main()
+{
+    return 0;
+}
+
---
+++ BlockIdConverter/stdafx.h
@@ -0,0 +1,11 @@
+// stdafx.h: dołącz plik do standardowych systemowych plików dołączanych,
+// lub specyficzne dla projektu pliki dołączane, które są często wykorzystywane, ale
+// są rzadko zmieniane
+//
+
+#pragma once
+
+#include "targetver.h"
+#include <vector>
+
+// TODO: W tym miejscu odwołaj się do dodatkowych nagłówków wymaganych przez program
